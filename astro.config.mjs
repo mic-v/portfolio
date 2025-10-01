c78a3589c6e04fadab50634903a4aee62a113ba6
Michael Victorino <michaelvictorino@gmail.com>
2025-10-01 00:56:44 -0400

    changed astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://mic-v.github.io',
-    base: '/portfolio',
+    site: 'https://michaelvictorino.ca',
 });
